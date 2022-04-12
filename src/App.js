dbcee6b9107d3544819ad736833089f918d85205
billyandryus <billyandrys@gmail.com>
2022-04-12 15:16:34 -0500

    useState tree input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 
 
 import { useForm } from './useForm';
-
+import { useEffect } from 'react'
 
 const App = ()=>{
-  const [values, handleChange] = useForm({email:'', password:''})
+  const [values, handleChange] = useForm({
+    email:'',
+    password:'',
+    firstName:''  
+  })
+
+
+  useEffect(()=>{
+    console.log('cars')
+  }, [values])
   
   return (
     <>
+    <input name='firstName' value={values.firstName} type='text' onChange={handleChange}/>
       <input name='email' value={values.email} type='text' onChange={handleChange}/>
       <input type='password'  value={values.password} name='password' onChange={handleChange}/>
     </>
