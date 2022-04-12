cad66248b5ce9e9446a6245a0b5051bdf14f92b0
billyandryus <billyandrys@gmail.com>
2022-04-12 13:01:01 -0500

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+import {useState} from 'react'
+
+
+const App = ()=>{
+  const [count, setCount] = useState(10)
+  const [count2, setCount2] = useState(20)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <button onClick={() => {
+        setCount(e=>e+1)
+        setCount2(i=>i+1)
+      }}
+      
+    >+</button>
+      <div>count : {count}</div>
+      <div>count 2  : {count2}</div>
+    </>
+  )
 }
+
 
 export default App;
